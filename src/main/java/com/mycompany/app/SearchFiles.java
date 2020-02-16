7d7f8fe61a0d6950021a77efe6fe1d404a159d52
Andrew D <donegaan@tcd.ie>
2020-02-16 15:35:12 +0000

    SearchFiles works, indexing not working quite right

--- src/main/java/com/mycompany/app/SearchFiles.java
+++ src/main/java/com/mycompany/app/SearchFiles.java
@@ -19,6 +19,7 @@ package com.mycompany.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -35,6 +36,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
 
 /** Simple command-line based search demo. */
@@ -53,12 +55,12 @@ public class SearchFiles {
 
         String index = "index";
         String field = "contents";
-        String output_filepath = "output.txt";
         String queries = null;
         int repeat = 0;
         boolean raw = false;
         String queryString = null;
         int hitsPerPage = 10;
+        int queryId = 1;
 
         for (int i = 0; i < args.length; i++) {
             if ("-index".equals(args[i])) {
@@ -92,165 +94,72 @@ public class SearchFiles {
         IndexSearcher searcher = new IndexSearcher(reader);
         Analyzer analyzer = new StandardAnalyzer();
 
+        // ---------------- Choose Scoring method ----------------
+
+        // Vector Space Model
+        // searcher.setSimilarity(new ClassicSimilarity());
+
+        // BM25
+        searcher.setSimilarity(new BM25Similarity());
+
         BufferedReader in = null;
         if (queries != null) {
             in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
         } else {
             in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
         }
+
         MultiFieldQueryParser parser = new MultiFieldQueryParser(
                 new String[] { "title", "author", "bibliography", "words" }, analyzer);
+        PrintWriter writer = new PrintWriter("/Users/andrewdonegan/GitHub/info_ret/my-app/output.txt", "UTF-8");
 
         String queryLines = "";
-        while (true) {
-            if (queries == null && queryString == null) { // prompt the user
-                System.out.println("Enter query: ");
-            }
+        Boolean first = true;
+        String line = "";
 
-            String line = queryString != null ? queryString : in.readLine();
-
-            if (line == null || line.length() == -1) {
-                break;
-            }
-
-            line = line.trim();
-            if (line.length() == 0) {
-                break;
-            }
+        // Code credit:
+        // https://github.com/nating/lucene-search-engine/blob/master/luceneapp/src/main/java/com/mycompany/luceneapp/SearchFiles.java
+        while ((line = in.readLine()) != null) {
 
             if (line.substring(0, 2).equals(".I")) {
-                line = in.readLine();
-                if (line.substring(0, 2).equals(".W")) {
-                    line = in.readLine();
-                    while (!line.substring(0, 2).equals(".I") && line != null) {
-                        queryLines = queryLines + " " + line;
-                        line = in.readLine();
-                    }
+                if (!first) {
+                    Query query = parser.parse(QueryParser.escape(queryLines));
+                    doSearch(searcher, query, queryId, writer);
+                    queryId++;
+                } else {
+                    first = false;
                 }
-            }
-
-            Query query = parser.parse(QueryParser.escape(queryLines.trim()));
-            System.out.println("Searching for: " + query.toString(field));
-
-            if (repeat > 0) { // repeat & time as benchmark
-                Date start = new Date();
-                for (int i = 0; i < repeat; i++) {
-                    searcher.search(query, 100);
-                }
-                Date end = new Date();
-                System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
-            }
-
-            doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
-
-            if (queryString != null) {
-                break;
+                queryLines = "";
+            } else {
+                queryLines += " " + line;
             }
         }
+
+        Query query = parser.parse(QueryParser.escape(queryLines));
+        doSearch(searcher, query, queryId, writer);
+
+        writer.close();
         reader.close();
     }
 
-    /**
-     * This demonstrates a typical paging search scenario, where the search engine
-     * presents pages of size n to the user. The user can then go to the next page
-     * if interested in the next hits.
-     *
-     * When the query is executed for the first time, then only enough results are
-     * collected to fill 5 result pages. If the user wants to page beyond this
-     * limit, then the query is executed another time and all hits are collected.
-     *
-     */
-    public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, int hitsPerPage,
-            boolean raw, boolean interactive) throws IOException {
-
-        // Collect enough docs to show 5 pages
-        TopDocs results = searcher.search(query, 5 * hitsPerPage);
+    // Code credit:
+    // https://github.com/nating/lucene-search-engine/blob/master/luceneapp/src/main/java/com/mycompany/luceneapp/SearchFiles.java
+    public static void doSearch(IndexSearcher searcher, Query query, Integer queryId, PrintWriter writer)
+            throws IOException {
+        TopDocs results = searcher.search(query, 1400);
         ScoreDoc[] hits = results.scoreDocs;
 
-        int numTotalHits = Math.toIntExact(results.totalHits.value);
-        System.out.println(numTotalHits + " total matching documents");
-
-        int start = 0;
-        int end = Math.min(numTotalHits, hitsPerPage);
-
-        while (true) {
-            if (end > hits.length) {
-                System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits
-                        + " total matching documents collected.");
-                System.out.println("Collect more (y/n) ?");
-                String line = in.readLine();
-                if (line.length() == 0 || line.charAt(0) == 'n') {
-                    break;
-                }
-
-                hits = searcher.search(query, numTotalHits).scoreDocs;
-            }
-
-            end = Math.min(hits.length, start + hitsPerPage);
-
-            for (int i = start; i < end; i++) {
-                if (raw) { // output raw format
-                    System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
-                    continue;
-                }
-
-                Document doc = searcher.doc(hits[i].doc);
-                String path = doc.get("path");
-                if (path != null) {
-                    System.out.println((i + 1) + ". " + path);
-                    String title = doc.get("title");
-                    if (title != null) {
-                        System.out.println("   Title: " + doc.get("title"));
-                    }
-                } else {
-                    System.out.println((i + 1) + ". " + "No path for this document");
-                }
-
-            }
-
-            if (!interactive || end == 0) {
-                break;
-            }
-
-            if (numTotalHits >= end) {
-                boolean quit = false;
-                while (true) {
-                    System.out.print("Press ");
-                    if (start - hitsPerPage >= 0) {
-                        System.out.print("(p)revious page, ");
-                    }
-                    if (start + hitsPerPage < numTotalHits) {
-                        System.out.print("(n)ext page, ");
-                    }
-                    System.out.println("(q)uit or enter number to jump to a page.");
-
-                    String line = in.readLine();
-                    if (line.length() == 0 || line.charAt(0) == 'q') {
-                        quit = true;
-                        break;
-                    }
-                    if (line.charAt(0) == 'p') {
-                        start = Math.max(0, start - hitsPerPage);
-                        break;
-                    } else if (line.charAt(0) == 'n') {
-                        if (start + hitsPerPage < numTotalHits) {
-                            start += hitsPerPage;
-                        }
-                        break;
-                    } else {
-                        int page = Integer.parseInt(line);
-                        if ((page - 1) * hitsPerPage < numTotalHits) {
-                            start = (page - 1) * hitsPerPage;
-                            break;
-                        } else {
-                            System.out.println("No such page");
-                        }
-                    }
-                }
-                if (quit)
-                    break;
-                end = Math.min(numTotalHits, start + hitsPerPage);
-            }
+        // Write the results for each hit
+        for (int i = 0; i < hits.length; i++) {
+            Document doc = searcher.doc(hits[i].doc);
+            /*
+             * Write the results in the format expected by trec_eval: | Query Number | 0 |
+             * Document ID | Rank | Score | "EXP" |
+             * (https://stackoverflow.com/questions/4275825/how-to-evaluate-a-search-
+             * retrieval-engine-using-trec-eval)
+             */
+            writer.println(
+                    queryId + " 0 " + doc.get("path").replace(".I ", "") + " " + i + " " + hits[i].score + " EXP");
         }
     }
 }
